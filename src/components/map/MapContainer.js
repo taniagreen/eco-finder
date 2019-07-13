1beafc60165d4a241431b1363aa964dccfc757b3
tania_green <tania.tkachuk@finder.com>
2019-07-13 14:08:35 +1000

    fix(map): fix map

--- src/components/map/MapContainer.js
+++ src/components/map/MapContainer.js
@@ -1,41 +1,35 @@
 import React, { Component } from 'react';
-import { GoogleMapReact } from 'google-maps-react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './map.css';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-export default class MapContainer extends Component {
+class MapContainer extends Component {
   static defaultProps = {
     center: {
       lat: -33.890660,
       lng: 151.212970
     },
-    zoom: 11
+    zoom: 11,
+    google: "yes",
   };
   
   render() {
     return (
       <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyBqxLAkaXcAqIkPkRYRvIs76y7WKPtFAC4' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          // google={props.google}
-          // zoom={8}
-          // initial={{ lat: -33.890660, lng: 151.212970}}
-          // className='map'
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>    
+        <Map
+        google={this.props.google}
+        zoom={14}
+        initialCenter={{
+         lat: -33.890660,
+         lng: 151.212970
+        }}
+      />
       </div>
     );
   }
 }
 
-// export default GoogleApiWrapper({
-//   apiKey: 'AIzaSyBqxLAkaXcAqIkPkRYRvIs76y7WKPtFAC4'
-// })(MapContainer);
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBqxLAkaXcAqIkPkRYRvIs76y7WKPtFAC4'
+})(MapContainer);
---
+++ src/response.js
@@ -0,0 +1,10 @@
+export const categoriesResponseMock = {
+    "keepCups": [
+        {
+            "name": "Cafe 70",
+            "latitude": "-33.884261",
+            "longitude": "151.211476",
+            "image": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjGsJKg87DjAhUSWysKHVM-B5MQjRx6BAgBEAU&url=https%3A%2F%2Fwww.zomato.com%2Fsydney%2Fcafe-70-surry-hills&psig=AOvVaw2AFMJzS9F-VfcQHWDJj7ox&ust=1563073108638434"
+        }
+    ],
+}
