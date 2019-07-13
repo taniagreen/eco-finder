83c6578d7b0f6b1bda647cd19e3ec434cc1ee84e
tania_green <tania.tkachuk@finder.com>
2019-07-13 10:27:30 +1000

    feat(app): create app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './images/eco-finder-logo.ico';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Eco Finder
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
